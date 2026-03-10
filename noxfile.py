e229a52c030c2ded611e588a860605367689b614
Constantin Müller <info@mueller-constantin.de>
2026-03-10 18:44:45 +0100

    build: initial commit

---
+++ noxfile.py
@@ -0,0 +1,28 @@
+"""
+Contains nox sessions.
+"""
+
+import nox
+
+@nox.session
+def tests(session):
+    """
+    Run tests.
+    """
+    session.run("poetry", "install", "-E", "pandas", external=True)
+    session.run("pytest")
+
+@nox.session
+def lint(session):
+    """
+    Run linter.
+    """
+    session.run("poetry", "install", "-E", "pandas", external=True)
+    session.run("pylint", "qvd4nifi", "tests")
+
+@nox.session
+def build(session):
+    """
+    Build package.
+    """
+    session.run("poetry", "build", external=True)
